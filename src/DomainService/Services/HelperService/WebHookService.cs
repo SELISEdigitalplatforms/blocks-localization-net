c2231d6dee7d381d2c8eb0422680f0578de80141
Md. Rifath Sahriar <Anubis1080p@gmail.com>
2025-11-17 12:44:24 +0600

    feat: add ConfigController and implement webhook saving functionality

--- src/DomainService/Services/HelperService/WebHookService.cs
+++ src/DomainService/Services/HelperService/WebHookService.cs
@@ -1,4 +1,6 @@
 using DomainService.Repositories;
+using DomainService.Shared;
+using DomainService.Shared.Entities;
 using Microsoft.Extensions.Logging;
 
 namespace DomainService.Services.HelperService
@@ -22,6 +24,27 @@ namespace DomainService.Services.HelperService
             if (webhook == null || webhook.IsDisabled) return true;
 
             return await _httpHelperServices.MakeHttpRequestForWebhook(payload, webhook);
+        }
+
+        public async Task<ApiResponse> SaveWebhookAsync(BlocksWebhook webhook)
+        {
+            //var validationResult = await _validator.ValidateAsync(webhook);
+
+            //if (!validationResult.IsValid)
+            //    return new ApiResponse(string.Empty, validationResult.Errors);
+
+            try
+            {
+                //var repoModule = await MappedIntoRepoModuleAsync(webhook);
+                await _blocksWebhookRepository.SaveAsync(webhook);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error while saving BlocksWebhook {errorMessage} : {StackTrace}", ex.Message, ex.StackTrace);
+                return new ApiResponse(ex.Message);
+            }
+
+            return new ApiResponse();
         }
     }
 }
