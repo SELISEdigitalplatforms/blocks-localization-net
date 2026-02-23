b4cf982b07659a9b4efe0fa05e5220ece6657a5b
Koushik Ghosh <koushik.ghosh@selise.ch>
2026-02-23 13:23:57 +0600

    add dynamic support for rabbitMQ

--- src/DomainService/Shared/Utilities/Constants.cs
+++ src/DomainService/Shared/Utilities/Constants.cs
@@ -9,19 +9,58 @@ namespace DomainService.Utilities
         public const string TranslateAllKeysQueue = "blocks_uilm_translate_all_keys_listener";
         public const string TranslateBlocksLanguageKeyQueue = "blocks_uilm_translate_blocks_language_key_listener";
         public const string EnvironmentDataMigrationQueue = "blocks_uilm_environment_data_migration_listener";
-        public static MessageConfiguration GetMessageConfiguration()
+
+        private const string DefaultProvider = "azure";
+        private const string RabbitMqProvider = "rabbitmq";
+
+
+        public static MessageConfiguration GetMessageConfiguration(string messageConnectionString)
+        {
+            var provider = GetProvider(messageConnectionString);
+
+            return provider switch
+            {
+                RabbitMqProvider => CreateRabbitMqConfiguration(),
+                _ => CreateAzureServiceBusConfiguration()
+            };
+        }
+
+        private static string GetProvider(string messageConnectionString)
+        {
+            if (Uri.TryCreate(messageConnectionString, UriKind.Absolute, out var uri))
+            {
+                if (uri.Scheme.Equals("amqp", StringComparison.OrdinalIgnoreCase) ||
+                    uri.Scheme.Equals("amqps", StringComparison.OrdinalIgnoreCase))
+                {
+                    return RabbitMqProvider;
+                }
+            }
+
+            return DefaultProvider;
+        }
+
+        private static MessageConfiguration CreateRabbitMqConfiguration()
+        {
+            return new MessageConfiguration
+            {
+                RabbitMqConfiguration = new RabbitMqConfiguration
+                {
+                    ConsumerSubscriptions = [ConsumerSubscription.BindToQueue(UilmQueue),
+                                             ConsumerSubscription.BindToQueue(UilmImportExportQueue),
+                                             ConsumerSubscription.BindToQueue(EnvironmentDataMigrationQueue),
+                                             ConsumerSubscription.BindToQueue(TranslateAllKeysQueue),
+                                             ConsumerSubscription.BindToQueue(TranslateBlocksLanguageKeyQueue)],
+                }
+            };
+        }
+
+        private static MessageConfiguration CreateAzureServiceBusConfiguration()
         {
             return new MessageConfiguration
             {
                 AzureServiceBusConfiguration = new AzureServiceBusConfiguration
                 {
-                    Queues = [
-                        Constants.UilmQueue,
-                        Constants.UilmImportExportQueue,
-                        Constants.EnvironmentDataMigrationQueue,
-                        Constants.TranslateAllKeysQueue,
-                        Constants.TranslateBlocksLanguageKeyQueue
-                    ],
+                    Queues = [UilmQueue, UilmImportExportQueue, EnvironmentDataMigrationQueue, TranslateAllKeysQueue, TranslateBlocksLanguageKeyQueue],
                     Topics = []
                 }
             };
