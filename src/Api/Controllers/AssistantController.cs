069b08ccfeda6d7dbddd6f7871aa8404a976567c
Rifath Sahriar <114345102+rifathsahriar@users.noreply.github.com>
2026-04-21 11:43:14 +0600

    Refactor AssistantController and SuggestLanguageRequest to include ChangeControllerContext and ProjectKey

--- src/Api/Controllers/AssistantController.cs
+++ src/Api/Controllers/AssistantController.cs
@@ -1,4 +1,5 @@
-﻿using DomainService.Services;
+﻿using Blocks.Genesis;
+using DomainService.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -10,16 +11,19 @@ namespace Api.Controllers
     public class AssistantController : ControllerBase
     {
         private readonly IAssistantService _assistantService;
+        private readonly ChangeControllerContext _changeControllerContext;
 
-        public AssistantController(IAssistantService assistantService)
+        public AssistantController(IAssistantService assistantService, ChangeControllerContext changeControllerContext)
         {
             _assistantService = assistantService;
+            _changeControllerContext = changeControllerContext;
         }
         
         [HttpPost]
         [Authorize]
         public async Task<IActionResult> GetTranslationSuggestion([FromBody] SuggestLanguageRequest request)
         {
+            _changeControllerContext.ChangeContext(request);
             var response = await _assistantService.SuggestTranslation(request);
             return StatusCode((int)HttpStatusCode.OK, new
             {
